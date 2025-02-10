5b2bf8fbce4fad22fa0dd43410877823aa3c960a
Diego <diegodrp01@gmail.com>
2025-02-10 04:49:29 -0300

    Feature: Video previews now show a play icon, and a (broken) duration text.

--- app/src/main/java/com/diegodrp/gallery/view/selected_album/VideoViewHolder.kt
+++ app/src/main/java/com/diegodrp/gallery/view/selected_album/VideoViewHolder.kt
@@ -2,6 +2,7 @@ package com.diegodrp.gallery.view.selected_album
 
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
+import com.diegodrp.gallery.R
 import com.diegodrp.gallery.databinding.ResPreviewVideoBinding
 import com.diegodrp.gallery.helpers.PreviewSizeCalculator
 import com.diegodrp.gallery.model.Media
@@ -27,5 +28,12 @@ class VideoViewHolder(
             .load(video.uri)
             .override(size.width, size.height)
             .into(videoBinding.ivPreviewIconVideo)
+
+        videoBinding.tvPreviewVideoLength.text = calculateVideoLength(video)
+    }
+
+    private fun calculateVideoLength(video: Video): String {
+        val length = video.duration / 61235f
+        return length.toString().replace(".", ":")
     }
 }
