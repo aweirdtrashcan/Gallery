f5a627a541a0041ea556edab540cbb7b1e78ff13
Diego <diegodrp01@gmail.com>
2025-02-10 21:29:22 -0300

    Bug fix: Video previews now show the correct video duration.

--- app/src/main/java/com/diegodrp/gallery/view/selected_album/VideoViewHolder.kt
+++ app/src/main/java/com/diegodrp/gallery/view/selected_album/VideoViewHolder.kt
@@ -2,7 +2,6 @@ package com.diegodrp.gallery.view.selected_album
 
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
-import com.diegodrp.gallery.R
 import com.diegodrp.gallery.databinding.ResPreviewVideoBinding
 import com.diegodrp.gallery.helpers.PreviewSizeCalculator
 import com.diegodrp.gallery.model.Media
@@ -29,11 +28,21 @@ class VideoViewHolder(
             .override(size.width, size.height)
             .into(videoBinding.ivPreviewIconVideo)
 
-        videoBinding.tvPreviewVideoLength.text = calculateVideoLength(video)
+        videoBinding.tvPreviewVideoLength.text = formatVideoDuration(video)
     }
 
-    private fun calculateVideoLength(video: Video): String {
-        val length = video.duration / 61235f
-        return length.toString().replace(".", ":")
+    private fun formatVideoDuration(video: Video): String {
+        val duration = video.duration / 1000
+
+        return if (duration > 3600) {
+            val minutes = (duration % 3600) / 60
+            val seconds = duration % 60
+            val hours = duration / 3600
+            String.format("%d:%d:%02d", hours, minutes, seconds)
+        } else {
+            val minutes = duration / 60
+            val seconds = duration % 60
+            String.format("%d:%02d", minutes, seconds)
+        }
     }
 }
